3e17e55f23a119ad9bad73c22c8f6286bab7a860
Annabel14-14 <dafe.ebele@gmail.com>
2023-04-14 03:34:05 +0100

    added the multiply function to version 2

--- test.py
+++ test.py
@@ -7,4 +7,13 @@ def github():
     print("sum of numbers is {sum_of_numbers}")
 
 
-github()
+def multiply_nums():
+    print("Welcome to Github")
+    #add two numbers
+    var =1
+    var2 = 5
+    sum_of_num=var*var2
+    print("sum of numbers is {sum_of_numbers}")
+
+multiply_nums()
+#github()
